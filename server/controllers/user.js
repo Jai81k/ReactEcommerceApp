54f17fc202a96084cba49a9ff0bf1dac691f3665
Jai K <dev.j81k@gmail.com>
2022-06-01 17:19:26 +0530

    verify firebase token

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,7 +1,7 @@
 
 exports.create = (req, res) => {
     console.log({...req.headers}.authtoken);
-    res.json({
+    res.status(200).json({
         status: 200,
         results: 'User is created successfully!'
     })
--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,8 +1,23 @@
+const admin = require('../_firebase');
 
-exports.verifyToken = (req, res, next) => {
-    //const token = req.headers;
-    console.log('Verifying the token');
 
-    // call the Callback function
-    next();
+exports.verifyToken = async (req, res, next) => {
+    const token = {...req.headers}.authtoken;
+    console.log('Verifying the token', token);
+    try {
+        // Authenticate with firebase
+        const firebaseUser = await admin.auth().verifyIdToken(token);
+
+        // Add to 'req' variable to access globally
+        req.user = firebaseUser;
+
+        // call the Callback function
+        next();
+
+    } catch (error) {
+        console.log('AuthError', error.message);
+        res.status(401).json({
+            error: `Authentication is failed. ${error.message}`
+        });
+    }
 }

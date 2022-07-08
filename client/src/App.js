b7cd195d2193741e6b79e3844696d170782aae42
Jai K <dev.j81k@gmail.com>
2022-07-08 23:29:18 +0530

    Admin Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {Routes, Route, useNavigate} from "react-router-dom";
+import {useDispatch} from 'react-redux';
 import {ToastContainer} from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import {useDispatch} from 'react-redux';
 
 import {auth} from './_firebase';
 import Home from "./pages/Home";
@@ -11,12 +11,33 @@ import Login from "./pages/auth/Login";
 import Header from "./components/nav/Header";
 import RegisterComplete from './pages/auth/RegisterComplete';
 import ForgotPassword from './pages/auth/ForgotPassword';
+import {createUser, getCurrentUser} from './api/User';
+//!@import {roleBasedRedirect} from './functions/Common';
+//@check import UserRoute from './components/routes/UserRoute';
+import AdminRoute from './components/routes/AdminRoute';
+import History from './pages/user/History';
+import Password from './pages/user/Password';
+import Wishlist from './pages/user/Wishlist';
+import AdminDashboard from './pages/admin/AdminDashboard';
+
+import Loader from './components/Loader';
+import "./App.css";
 
 
 const App = () => {
   // Check the user state
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  //const [showUI, setshowUI] = useState(false);
+
+  const roleBasedRedirect = (role) => {
+      if (role === 'admin') {
+          navigate('/admin/dashboard');
+      } else {
+          // Normal user
+          navigate('/user/history');
+      }
+  }
   
   useEffect(() => {
     // Get status from Firebase
@@ -26,36 +47,57 @@ const App = () => {
           const idTokenResult = await user.getIdTokenResult();
           //console.log('User: ', user);
 
-          // Dispatch this information to "Redux" userReducer's state.
-          dispatch({
-            type: 'LOGGED_IN_USER',
-            payload: {
-              email: user.email,
-              token: idTokenResult.token
-            }
-          });
+          // Get current user database details
+          getCurrentUser(idTokenResult.token).then((res) => {
+              console.log(res);
+              // Dispatch this information to maintain "Redux" userReducer's state while Page refresh.
+              dispatch({
+                type: 'LOGGED_IN_USER',
+                payload: {
+                    email: res.data.email,
+                    name: res.data.name,
+                    role: res.data.role,
+                    userId: res.data._id,
+                    token: idTokenResult.token
+                }
+              });  
 
-          // Redirect to Home Page
-          navigate('/');
-        } else {
+              // redirect
+              roleBasedRedirect(res.data.role);
+          });
+        }/*!} else {
           // Redirect to Login Page
           navigate('/login');
-        }
+        }*/
     });
 
   }, []);
-
+  
   return (  
     <>
-      <Header />
-      <ToastContainer />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>} />
-        <Route path="/register/complete" element={<RegisterComplete/>} />
-        <Route path="/forgot/password" element={<ForgotPassword />} />
-      </Routes>
+      
+      {//showUI && (
+        <>
+          <Header />
+          <ToastContainer />
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/register/complete" element={<RegisterComplete/>} />
+            <Route path="/forgot/password" element={<ForgotPassword />} />
+            <Route path="/user/history" element={<History />} />
+            <Route path="/user/password" element={<Password />} />
+            <Route path="/user/wishlist" element={<Wishlist />} />
+            <Route path="/admin/dashboard" element={<AdminRoute><AdminDashboard /></AdminRoute>} />
+            {/* <Route path="/admin/dashboard/*" element={<AdminWrapper />}>
+              <Route index element={<AdminDashboard />}/>
+              <Route index element={<AdminHistory />}/>
+            </Route> */}
+          </Routes>
+        </>
+      //)
+    }  
     </>
   );
 }
